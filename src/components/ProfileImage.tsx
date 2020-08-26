66624e80880e5989c027e9349f8092be59d5d0b4
Anders Nylund <anders@andersnylund.com>
2020-08-26 19:09:28 +0300

    Remove framer-motion from image (#7)

--- src/components/ProfileImage.tsx
+++ src/components/ProfileImage.tsx
@@ -1,44 +1,14 @@
-import React, { useState } from 'react';
-import { motion, Variants } from 'framer-motion';
-import { colors } from '../GlobalStyle';
+import React from 'react';
+import styled from 'styled-components';
 
-const imageVariants: Variants = {
-  open: {
-    borderRadius: '30%',
-    width: '100%',
-    transition: {
-      type: 'spring',
-      stiffness: 20,
-      restDelta: 1
-    }
-  },
-  closed: {
-    width: '100px',
-    borderRadius: '50%',
-    objectFit: 'cover',
-    cursor: 'pointer',
-    transition: {
-      type: 'spring',
-      stiffness: 400,
-      damping: 40
-    }
-  }
-};
+const ProfileImage = () => (
+  <Image src={process.env.PUBLIC_URL + '/profile.jpg'} alt="Profile Picture" />
+);
 
-const ProfileImage = () => {
-  const [isOpen, setOpen] = useState(false);
-  return (
-    <motion.img
-      animate={isOpen ? 'open' : 'closed'}
-      variants={imageVariants}
-      onTap={() => setOpen(!isOpen)}
-      whileHover={{
-        boxShadow: `${colors.darkPink} 2px 2px 4px`
-      }}
-      src={process.env.PUBLIC_URL + '/profile.jpg'}
-      alt="Profile Picture"
-    />
-  );
-};
+const Image = styled.img`
+  width: 100px;
+  border-radius: 50%;
+  object-fit: cover;
+`;
 
 export default ProfileImage;
